9079b426a9735ccf285f8585e74ba1cd326e2144
rafalopezgit <rafael.lopezgm@gmail.com>
2018-01-13 15:26:01 +0100

    first iteration done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,28 @@
 const express = require('express');
-const app = express();
 const Chuck  = require('chucknorris-io');
-const client = new Chuck();
 
+const app = express();
+const pepe = new Chuck();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+// First iteration
+app.get('/random', (req, res, next) => {
+  console.log('La request está llegando')
+  pepe.getRandomJoke()
+    .then((response) => {
+      // console.log(response)
+      // res.send(response.value)
+      res.render('index', {response})
+    }).catch((err) => {
+      // console.log(err)
+      console.log('Esto esta dando error')
+    });
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!')
+});
